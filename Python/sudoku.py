7f6123a9b959e8ed1fdc3bb30a6ee97ea089dca0
RYAN R MCCOPPIN <ryan.mccoppin.1.ctr@us.af.mil>
2022-05-10 09:21:42 -0400

    finished working sudoku.

--- Python/sudoku.py
+++ Python/sudoku.py
@@ -20,22 +20,30 @@ solution = [[5,3,4,6,7,8,9,1,2],
             [3,4,5,2,8,6,1,7,9]]
 
 import itertools
-allset = {1,2,3,4,5,6,7,8,9}
-dict = {}
-
-def updateDict(keys, goset, puzzle):
+def updateDict(keys, goset, puzzle, dict =dict(), twos=dict()):
     for each in keys:
+        valueBeGone = set()
         try:
             dict[each] = dict[each].intersection(goset)
         except KeyError:
             dict[each] = goset
         if len(dict[each]) == 1 and None not in each:
             puzzle[each[0]][each[1]] = dict[each].pop()
+        if len(dict[each]) == 2 and None not in each:
+            twos[each] = dict[each]
+            for k, v in twos.items():
+                if dict[each] == v and each != k:
+                    valueBeGone = valueBeGone.union(v)
+                    # if each[0]==k[0]:
+                    #     for k,v in dict.items():
+                            
+                    # if each[1] ==k[1]:
+
 
 def threeByThree(puzzle, fi, fj):
     i = 3*(fi//3)
     j = 3*(fj//3)
-    items = allset - {
+    items = {1,2,3,4,5,6,7,8,9} - {
         puzzle[i][j],   puzzle[i][j+1],   puzzle[i][j+2],
         puzzle[i+1][j], puzzle[i+1][j+1], puzzle[i+1][j+2],
         puzzle[i+2][j], puzzle[i+2][j+1], puzzle[i][j+2]
@@ -49,40 +57,39 @@ def threeByThree(puzzle, fi, fj):
     return puzzle
 
 def colRow(puzzle, fi, fj):
-    global allset
-    cols = []
-    for i in range(fi, len(puzzle) if not fi else fi+1): # each row
-        updateDict([(i,j) for j in range(0,9) if puzzle[i][j] == 0], \
-            allset - set(puzzle[i]), puzzle )
-        if not fi:
-            for j, el in enumerate(puzzle[i]):
-                if len(cols) < j+1:
-                    cols.append([])
-                cols[j].append(el)
-    for j in range(fj, len(cols)):
-        updateDict([(i,j) for i in range(0,9) if puzzle[i][j] == 0], \
-            allset - set(cols[j]), puzzle)
+    updateDict([(fi,j) for j in range(0,9) if puzzle[fi][j] == 0], \
+        {1,2,3,4,5,6,7,8,9} - set(puzzle[fi]), puzzle )
+    updateDict([(i,fj) for i in range(0,9) if puzzle[i][fj] == 0], \
+        {1,2,3,4,5,6,7,8,9} - set([puzzle[i][fj] for i in range(0,9)]), puzzle)
     return puzzle
 
-def next0(puzzle, i):
-    for i in range(len(puzzle)):
+def nextFind(puzzle):
+    i,j = 0,0
+    while True:
         try:
-            j = puzzle[i].index(0)
-            return i,j
+            offj = puzzle[i][j:].index(0)
+            j = j + offj
+            yield i, j
+            j += 1
         except ValueError:
-            pass
-    return 0, 0
+            j = 0
+            i = (i + 1) % len(puzzle)
+            yield i, j
+        i = (i + j//9) % len(puzzle)
+        j = j % len(puzzle[i])
 
 def sudoku(puzzle):
     global allset
     i, j = 0, 0
     count = list(itertools.chain(*puzzle)).count(0)
+    gen = nextFind(puzzle)
     while count > 0:
+        i,j = next(gen)
         puzzle = colRow(puzzle, i,j)
         puzzle = threeByThree(puzzle, i,j)
-        i,j = next0(puzzle, i)
-        count = list(itertools.chain(*puzzle)).count(0)
-
+        count -= 1
+        if count <= 3:
+            count = list(itertools.chain(*puzzle)).count(0)
     return puzzle
 
 print(sudoku(puzzle))
