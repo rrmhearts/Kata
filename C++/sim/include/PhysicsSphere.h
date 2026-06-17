988365ce842d11416dbcd2000e86015f6a6eabe5
Ryan McCoppin <ryan.mccoppin.1.ctr@afrl.af.mil>
2026-06-17 16:24:57 -0400

    keeping comments

--- C++/sim/include/PhysicsSphere.h
+++ C++/sim/include/PhysicsSphere.h
@@ -3,17 +3,32 @@
 
 class PhysicsSphere {
 public:
+    // Spatial properties using GLM vectors
     glm::vec3 position;
     glm::vec3 velocity;
     float radius;
+
+    // Physical properties
     float mass;
-    float restitution;
+    float restitution; // Bounciness (0.0 = perfectly inelastic, 1.0 = perfectly elastic)
     glm::vec3 color; // New: Added for rendering
 
+
+    // Constructor
     PhysicsSphere(glm::vec3 pos, float r, float m, float rest, glm::vec3 col)
         : position(pos), velocity(0.0f), radius(r), mass(m), restitution(rest), color(col) {}
 
+    // Update the sphere's position based on its velocity and elapsed time (dt)
     void update(float dt) {
+        // Simple Euler integration
         position += velocity * dt;
+    }
+
+    // Abstracted draw function
+    // This is absconded by SphereRenderer
+    void draw() {
+        // In a real application, you would generate a model matrix here:
+        // glm::mat4 model = glm::translate(glm::mat4(1.0f), position);
+        // Then pass the model matrix to your OpenGL shader and draw the sphere mesh.
     }
 };
